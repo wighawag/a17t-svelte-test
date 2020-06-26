5dfe4c0f87e62377e88587c456ce329e4b106abf
Wighawag <wighawag@gmail.com>
2020-06-26 18:12:25 +0100

    tailwind only

--- src/App.svelte
+++ src/App.svelte
@@ -4,28 +4,6 @@
   export let name;
 </script>
 
-<style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
-
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
-</style>
-
 <Tailwindcss />
 <main>
   <h1>Hello {name}!</h1>
@@ -34,4 +12,8 @@
     <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
     to learn how to build Svelte apps.
   </p>
+
+  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">one</button>
+
+  <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">two</button>
 </main>
